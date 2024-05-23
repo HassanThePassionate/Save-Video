1656cd6a5478a638fe3e31906acf4115949198ab
HassanThePassionate <maqsoodia777@gmail.com>
2024-05-23 17:16:29 +0500

    Update

---
+++ components/Contact.js
@@ -0,0 +1,96 @@
+"use client";
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
+import { useState } from "react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+const Contact = () => {
+  const [lastClicked, setLastClicked] = useState(null);
+
+  const handleClick = (buttonName) => {
+    setLastClicked(buttonName);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className='max-w-2xl  mx-auto my-4 md:my-12 space-y-8'>
+      <div>
+        <h2 className='text-center text-6xl font-bold'>Contact Us</h2>
+        <p className='text-center mt-6 text-[20px] text-[#71717a] mb-[20px] '>
+          We're here to provide the support you need. Whether you have
+          questions, comments, or need assistance, we are ready to help.
+        </p>
+      </div>
+      <Card className='p-[16px]'>
+        <CardContent>
+          <form className='flex flex-col gap-6 mt-5' onSubmit={handleSubmit}>
+            <div>
+              <label>Name</label>
+              <Input placeholder='Your name' type='email' className='mt-3' />
+            </div>
+            <div>
+              <label>E-mail address</label>
+              <Input placeholder='Your e-mail address' className='mt-3' />
+            </div>
+            <div className='flex gap-3'>
+              <Button
+                variant='outline'
+                style={{
+                  backgroundColor:
+                    lastClicked === "General Contact" ? "black" : "",
+                  color: lastClicked === "General Contact" ? "white" : "",
+                }}
+                onClick={() => handleClick("General Contact")}
+              >
+                General Contact
+              </Button>
+              <Button
+                variant='outline'
+                style={{
+                  backgroundColor: lastClicked === "Feedback" ? "black" : "",
+                  color: lastClicked === "Feedback" ? "white" : "",
+                }}
+                onClick={() => handleClick("Feedback")}
+              >
+                Feedback
+              </Button>
+              <Button
+                variant='outline'
+                style={{
+                  backgroundColor: lastClicked === "Bug Report" ? "black" : "",
+                  color: lastClicked === "Bug Report" ? "white" : "",
+                }}
+                onClick={() => handleClick("Bug Report")}
+              >
+                Bug Report
+              </Button>
+              <Button
+                variant='outline'
+                style={{
+                  backgroundColor:
+                    lastClicked === "Feature Request" ? "black" : "",
+                  color: lastClicked === "Feature Request" ? "white" : "",
+                }}
+                onClick={() => handleClick("Feature Request")}
+              >
+                Feature Request
+              </Button>
+            </div>
+            <div>
+              <label>Message</label>
+              <Textarea placeholder='Enter your message' className='mt-3' />
+            </div>
+            <div className='flex items-center justify-center'>
+              <Button>Submit</Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Contact;
