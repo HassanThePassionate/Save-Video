9f247fd4bd42b9b225c1fd1e8c4314d2254c0832
HassanThePassionate <maqsoodia777@gmail.com>
2024-05-29 14:02:29 +0500

    Update

--- components/hero/UserList.js
+++ components/hero/UserList.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import UserItem from "./UserItem";
 import { Button } from "../ui/button";
-import { ArrowDown, ChevronRight } from "lucide-react";
+import { ChevronRight, ChevronUp } from "lucide-react";
 
 const UserList = () => {
   const item = [
@@ -99,6 +99,9 @@ const UserList = () => {
   const loadMore = () => {
     setVisibleItems((prevVisibleItems) => prevVisibleItems + 5);
   };
+  const showless = () => {
+    setVisibleItems((prevVisibleItems) => prevVisibleItems - 10);
+  };
   return (
     <>
       <div className='grid'>
@@ -111,6 +114,14 @@ const UserList = () => {
           <Button onClick={loadMore} variant='outline' className='mt-3 w-fit '>
             View more
             <ChevronRight size={16} className='ml-1' />
+          </Button>
+        </div>
+      )}
+      {visibleItems == item.length && (
+        <div className='flex items-center justify-center'>
+          <Button onClick={showless} variant='outline' className='mt-3 w-fit '>
+            Show less
+            <ChevronUp size={16} className='ml-1' />
           </Button>
         </div>
       )}
