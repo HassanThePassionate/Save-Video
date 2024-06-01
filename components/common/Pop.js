1c146f07eab802be35e6a11ad1e33522687ea75b
alihassan8989 <maqsoodia777@gmail.com>
2024-06-01 16:36:03 +0500

    Update

--- components/common/Pop.js
+++ components/common/Pop.js
@@ -73,71 +73,71 @@ const Pop = ({ sliderValue, formatTime }) => {
     <>
       <div>
         <AlertDialog>
-          <AlertDialogTrigger className='relative'>
-            <Button className='flex gap-2'>
+          <AlertDialogTrigger className="relative">
+            <Button className="flex gap-2">
               <VideoIcon />
               Download
             </Button>
             <Link
-              href='#'
-              className='absolute left-0 right-0 mt-2.5  flex justify-center cursor-pointer'
+              href="#"
+              className="absolute left-0 right-0 mt-2.5  flex justify-center cursor-pointer"
             >
-              <Badge variant='secondary'>{formatduration}</Badge>
+              <Badge variant="secondary">{formatduration}</Badge>
             </Link>
           </AlertDialogTrigger>
-          <AlertDialogContent className='mb-3 overflow-y-auto max-h-[calc(100vh-4rem)]'>
-            <AlertDialogCancel className='rounded-full border border-black dark:border-white p-1'>
-              <X />
-            </AlertDialogCancel>
-            <div className='flex justify-between items-center gap-10 mb-6 flex-col sm:flex-row'>
+          <AlertDialogContent className="mb-3  overflow-y-auto max-h-[calc(100vh-4rem)]">
+            <div className="flex justify-between items-center gap-10 mb-6 flex-col sm:flex-row mt-3">
               <div>
-                <Image src='/img.webp' alt='img' width={365} height={365} />
+                <Image src="/img.webp" alt="img" width={365} height={365} />
               </div>
               <div>
                 <AlertDialogHeader>
                   <AlertDialogTitle>Browser Extension</AlertDialogTitle>
                   <AlertDialogDescription>
-                    <div className='flex items-center gap-1 cursor-pointer'>
-                      <div className='px-[10px] py-[2px] dark:bg-[#555] bg-[#F6F6F7] rounded'>
-                        <span className='text-black font-semibold dark:text-white'>
+                    <div className="flex items-center gap-1 cursor-pointer">
+                      <div className="px-[10px] py-[2px] dark:bg-[#555] bg-[#F6F6F7] rounded">
+                        <span className="text-black font-semibold dark:text-white">
                           Chrome Extension
                         </span>
                       </div>
-                      <div className='px-[10px] py-[2px] dark:bg-[#555] dark:text-white bg-[#F6F6F7] rounded'>
-                        <span className='text-black font-semibold dark:text-white'>
+                      <div className="px-[10px] py-[2px] dark:bg-[#555] dark:text-white bg-[#F6F6F7] rounded">
+                        <span className="text-black font-semibold dark:text-white">
                           Free & Unlimited
                         </span>
                       </div>
                     </div>
                   </AlertDialogDescription>
-                  <p className='mb-[10px]'>
+                  <p className="mb-[10px]">
                     This component is built using shadcn/ui&apos;s dialog and
                     drawer component, which is built on top of Vaul.
                   </p>
-                  <p className='mb-[10px]'>
+                  <p className="mb-[10px]">
                     It shows a dialog modal for desktop view and a bottom drawer
                     for mobile view.
                   </p>
                 </AlertDialogHeader>
               </div>
             </div>
-            <div className='divider div-transparent mb-4'></div>
-            <AlertDialogFooter className='gap-2 sm:gap-0'>
+            <div className="divider div-transparent mb-4 "></div>
+            <AlertDialogFooter className="gap-2 sm:gap-0">
               {sortedBtn.map((e, index) => (
-                <AlertDialogAction variant='outline' key={e.img}>
-                  <div className='flex items-center gap-2'>
-                    <Image src={e.img} alt='img' width={22} height={22} />
-                    <span className='text-[15px]'>
+                <AlertDialogAction variant="outline" key={e.img}>
+                  <div className="flex items-center gap-2">
+                    <Image src={e.img} alt="img" width={22} height={22} />
+                    <span className="text-[15px]">
                       {index === 0 ? `Add to ${e.label}` : e.label}
                     </span>
                   </div>
                 </AlertDialogAction>
               ))}
             </AlertDialogFooter>
-            <p className='mt-2 text-[12px]'>
+            <p className="mt-2 text-[12px]">
               Foxified extension is needed to make YouTube Video Downloader work
               properly.
             </p>
+            <AlertDialogCancel className="rounded-full border  border-black dark:border-white p-1 absolute right-3 top-3">
+              <X />
+            </AlertDialogCancel>
           </AlertDialogContent>
         </AlertDialog>
       </div>
